9ddebe7613f2a8cda21ffa55d5dc7b5735847075
Eamon <eamon0989@gmail.com>
2021-12-20 16:54:48 +0100

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,62 +1,82 @@
-// import { Text } from './components/text';
 import wordsService from './services/words'
 import React, { useState, useEffect } from 'react'
 
-const Word = function({ word }) {
-  return <span>{word}</span>
+
+const Word = function ({ word, status }) {
+  return <span className={status + ' word'}>{word}</span>
+};
+
+
+const Phrase = function ({ phrase, status, markedWords }) {
+  const parts = phrase.split(' ');
+
+  return (
+    <span className={status + ' phrase'}>
+      {
+        parts.map((word, index, array) => <><Word key={word + index} word={word} status={markedWords[word.toLowerCase()]} />{index === array.length - 1 ? '' : ' '}</>)
+      }
+    </span>
+  )
+};
+
+
+const Paragraph = function({ paragraph, words }) {
+  const markedWords = {};
+  words.forEach(obj => markedWords[obj.word] = obj.state);
+
+  const phraseFinder = `(${Object.keys(markedWords).filter(key => key.split(' ').length > 1).join('|')})`;
+  const wordFinder = `(?<words>[\\p{Letter}\\p{Mark}'-]+)`;
+  const noWordFinder = `(?<nowords>[^\\p{Letter}\\p{Mark}'-]+)`;
+  
+  const phraseRegExp = new RegExp(phraseFinder, 'gui');
+  const wordRegExp = new RegExp(wordFinder, 'gui');
+  const tokenRegExp = new RegExp(`${phraseFinder}|${wordFinder}|${noWordFinder}`, 'gui');  
+
+  const tokens = paragraph.match(tokenRegExp);
+ 
+  return (
+    <p>
+      {
+        tokens.map((token, index) => {
+          if (token.match(phraseRegExp)) return <Phrase key={token + index} phrase={token} markedWords={markedWords} status={markedWords[token.toLowerCase()]} />;
+          if (token.match(wordRegExp)) return <Word key={token + index} word={token} status={markedWords[token.toLowerCase()]} />;
+          return <span key={token + index}>{token}</span>;
+        })
+      }
+    </p>
+  );  
 }
 
-const Text = function({text}) {
-  // console.log(text)
-  // const words = text.split(' ');
-  // console.log('words')
-  // console.log(words)
+
+const TextBody = function ({ text, words }) {
+  const paragraphs = text?.split('\n');
+
   return (
     <div>
-      {text}
+      {
+        paragraphs.map((paragraph, index) => <Paragraph key={index} paragraph={paragraph} words={words} />)
+      }
     </div>
-  )
-}
+  );
+};
 
 function App() {
-  const [text, setText] = useState([]);
+  const [text, setText] = useState('');
   const [words, setWords] = useState([]);
-  const [phrases, setPhrases] = useState([]);
 
-  // const getPhrases = function() {
-  //   console.log('gettingphrases')
-  //   console.log(phrases)
-  //   words.forEach(word => {
-  //     if (/ /.test(word.word)) {
-  //       // console.log(word)
-  //       console.log(phrases)
-  //       // const updatedPhrases = [...phrases];
-  //       // phrases.push(word);
-  //       // console.log('updatedPhrases')
-  //       // console.log(updatedPhrases)
-  //       setPhrases([...phrases, word])
-  //     }
-  //   })
-
-  //   console.log(phrases)
-  // }
-
-  useEffect(() => {
+  const getWordsAndText = function() {
     wordsService.getText().then(text => setText(text.body))
     wordsService.getWordsFromText().then(words => setWords(words))
-  }, [])
+  }
 
-  // useEffect(getPhrases, [words])
-  // console.log(phrases)
-  // console.log(text)
+  useEffect(getWordsAndText, [])
 
   return (
     <>
-      <p>{'Insert text here'}</p>
-      <Text text={text}/>
-      {/* {words} */}
+      {text && <TextBody text={text} words={words} />}
     </>
-  )
+  );
+
 }
 
 export default App;
--- src/services/words.js
+++ src/services/words.js
@@ -2,15 +2,30 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3000/api/words'
 
 const getWordsFromText = async function() {
-  const request = await axios.get(`${baseUrl}/text/1/user/1`);
-  return request.data;
+  // const request = await axios.get(`${baseUrl}/text/1/user/1`);
+  // return request.data;
+
+  return [
+    {id: 1, word: 'of course', state: 'familiar'},
+    {id: 3, word: 'across the road', state: 'learning'},
+    {id: 7, word: 'bareheaded', state: 'learned'},
+    {id: 9, word: 'carriages', state: 'learning'}
+  ]
 }
+
 
 const getText = async function() {
   console.log('getText')
   const request = await axios.get('http://localhost:3000/api/texts/1');
   console.log(request)
   return request.data;
+
+  // return [
+  //   {id: 1, word: 'of course', state: 'familiar'},
+  //   {id: 3, word: 'across the road', state: 'learning'},
+  //   {id: 7, word: 'bareheaded', state: 'learned'},
+  //   {id: 9, word: 'carriages', state: 'learning'}
+  // ]
 }
 
 export default {
