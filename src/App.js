b0cdd5258ff63ff5f80ed2614f2fb925df7bb84f
Eamon <eamon0989@gmail.com>
2021-12-20 12:46:26 +0100

    my working code

--- src/App.js
+++ src/App.js
@@ -1,84 +1,44 @@
+// import { Text } from './components/text';
 import wordsService from './services/words'
 import React, { useState, useEffect } from 'react'
 
-const Word = function ({ word, status }) {
-  return <span className={status + ' word'}>{word}</span>
-};
+const Text = function({text}) {
 
+  
+  // const getText = async function() {
+  //   const text = await wordsService.getText();
+  //   setText(text);
+  //   // return text;
+  // }
 
-const Phrase = function ({ phrase, status, markedWords }) {
-  const parts = phrase.split(' ');
-
+  // getText();
+  // console.log(text);
   return (
-    <span className={status + ' phrase'}>
-      {
-        parts.map((word, index, array) => <><Word key={word + index} word={word} status={markedWords[word.toLowerCase()]} />{index === array.length - 1 ? '' : ' '}</>)
-      }
-    </span>
+    <div>
+      <p>{'Insert text'}</p>
+      {text.title}
+      {text.body}
+    </div>
   )
-};
-
-
-const Paragraph = function({ paragraph, markedWords }) {
-  const phraseFinder = `(${Object.keys(markedWords).filter(key => key.split(' ').length > 1).join('|')})`;
-  const wordFinder = `(?<words>[\\p{Letter}\\p{Mark}'-]+)`;
-  const noWordFinder = `(?<nowords>[^\\p{Letter}\\p{Mark}'-]+)`;
-  const tokenRegExp = new RegExp(`${phraseFinder}|${wordFinder}|${noWordFinder}`, 'gui');
-  const phraseRegExp = new RegExp(phraseFinder, 'gui');
-  const wordRegExp = new RegExp(wordFinder, 'gui');
-
-  const tokens = paragraph.match(tokenRegExp);
- 
-  return (
-    <p>
-      {
-        tokens.map((token, index) => {
-          if (token.match(phraseRegExp)) return <Phrase key={token + index} phrase={token} markedWords={markedWords} status={markedWords[token.toLowerCase()]} />;
-          if (token.match(wordRegExp)) return <Word key={token + index} word={token} status={markedWords[token.toLowerCase()]} />;
-          return <span key={token + index}>{token}</span>;
-        })
-      }
-    </p>
-  );  
 }
 
 
-const TextBody = function ({ text, words }) {
-  console.log(text)
-  const paragraphs = text.split('\n');
-  const markedWords = {
-    'of course': 'learning',
-    'across the road': 'learning',
-    'bareheaded': 'learning',
-    'carriages': 'learning',
-  };
 
-  return (
-    <div>
-      {
-        paragraphs.map((paragraph, index) => <Paragraph key={index} paragraph={paragraph} markedWords={markedWords} />)
-      }
-    </div>
-  );
-};
-
-const App = function() {
+function App() {
   const [text, setText] = useState([]);
   const [words, setWords] = useState([]);
 
+
   useEffect(() => {
-    wordsService.getText().then(text => {
-      setText(text)
-      console.log(text)
-    })
+    wordsService.getText().then(text => setText(text))
     wordsService.getWordsFromText().then(words => setWords(words))
   }, [])
 
-  console.log(text);
+  console.log(words)
   return (
     <>
       <p>{'Insert text here'}</p>
-      <TextBody text={text} words={words}/>
+      <Text text={text}/>
       {/* {words} */}
     </>
   )
