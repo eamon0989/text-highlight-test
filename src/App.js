ceaed8170d3677378ed307087dbee7008174fa30
Eamon <eamon0989@gmail.com>
2021-12-20 17:34:21 +0100

    added cyclestate for known words

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import wordsService from './services/words'
 import React, { useState, useEffect } from 'react'
 
 
-const Word = function ({ word, status }) {
-  return <span className={status + ' word'}>{word}</span>
+const Word = function ({ word, status, cycleState }) {
+  // const cycleState = function(event) {
+  //   const word = event.target.textContent;
+  //   console.log(word)
+  // }
+
+  return <span onClick={cycleState} className={status + ' word'}>{word}</span>
 };
 
 
-const Phrase = function ({ phrase, status, markedWords }) {
+const Phrase = function ({ phrase, status, markedWords, cycleState }) {
   const parts = phrase.split(' ');
 
   return (
-    <span className={status + ' phrase'}>
+    <span onClick={cycleState} className={status + ' phrase'}>
       {
         parts.map((word, index, array) => <><Word key={word + index} word={word} status={markedWords[word.toLowerCase()]} />{index === array.length - 1 ? '' : ' '}</>)
       }
@@ -20,7 +25,7 @@ const Phrase = function ({ phrase, status, markedWords }) {
 };
 
 
-const Paragraph = function({ paragraph, words }) {
+const Paragraph = function({ paragraph, words, cycleState }) {
   const markedWords = {};
   words.forEach(obj => markedWords[obj.word] = obj.state);
 
@@ -38,8 +43,8 @@ const Paragraph = function({ paragraph, words }) {
     <p>
       {
         tokens.map((token, index) => {
-          if (token.match(phraseRegExp)) return <Phrase key={token + index} phrase={token} markedWords={markedWords} status={markedWords[token.toLowerCase()]} />;
-          if (token.match(wordRegExp)) return <Word key={token + index} word={token} status={markedWords[token.toLowerCase()]} />;
+          if (token.match(phraseRegExp)) return <Phrase cycleState={cycleState} key={token + index} phrase={token} markedWords={markedWords} status={markedWords[token.toLowerCase()]} />;
+          if (token.match(wordRegExp)) return <Word cycleState={cycleState} key={token + index} word={token} status={markedWords[token.toLowerCase()]} />;
           return <span key={token + index}>{token}</span>;
         })
       }
@@ -48,13 +53,13 @@ const Paragraph = function({ paragraph, words }) {
 }
 
 
-const TextBody = function ({ text, words }) {
+const TextBody = function ({ text, words, cycleState }) {
   const paragraphs = text?.split('\n');
 
   return (
     <div>
       {
-        paragraphs.map((paragraph, index) => <Paragraph key={index} paragraph={paragraph} words={words} />)
+        paragraphs.map((paragraph, index) => <Paragraph cycleState={cycleState} key={index} paragraph={paragraph} words={words} />)
       }
     </div>
   );
@@ -64,16 +69,37 @@ function App() {
   const [text, setText] = useState('');
   const [words, setWords] = useState([]);
 
+  const cycleState = function(event) {
+    const word = event.target.textContent;
+    const wordObj = words.filter(wordObj => wordObj.word === word);
+    if (wordObj.length > 0) {
+      const wordObject = wordObj[0];
+
+      if (wordObject.state === undefined) {
+        wordObject.state = 'learning';
+      } else if (wordObject.state === 'familiar') {
+        wordObject.state = 'learned';
+      } else if (wordObject.state === 'learned') {
+        wordObject.state = 'learning';
+      } else if (wordObject.state === 'learning') {
+        wordObject.state = 'familiar';
+      }
+
+      const updatedWords = [...words.filter(wordObj => wordObj.word !== word), wordObject];
+      setWords(updatedWords)
+    }
+  }
+
   const getWordsAndText = function() {
     wordsService.getText().then(text => setText(text.body))
     wordsService.getWordsFromText().then(words => setWords(words))
   }
 
   useEffect(getWordsAndText, [])
-
+  // console.log(words)
   return (
     <>
-      {text && <TextBody text={text} words={words} />}
+      {text && <TextBody cycleState={cycleState} text={text} words={words} />}
     </>
   );
 
